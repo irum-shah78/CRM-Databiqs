70895a17d7af102942d7c14ac0902a49d38174b6
Jawad Afzal <86598805+JawadAfzal123@users.noreply.github.com>
2024-10-04 02:41:00 +0500

    Add files via upload

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,6 @@
+const Loader = () => (
+  <div className="flex justify-center items-center h-64">
+    <div className="animate-spin rounded-full h-10 w-10 border-8 border-[#f3f3f3] border-t-8 border-t-[#7234D7]" ></div>
+  </div>
+);
+export default Loader;
---
+++ src/pages/jobcalculator/JobCalculator.jsx
@@ -0,0 +1,409 @@
+import React, { useRef, useState } from 'react';
+import axios from 'axios';
+import searchIcon from "../../assets/search.svg";
+import listView from "../../assets/listView.svg";
+import kanbanView from "../../assets/kanbanView.svg";
+import filter from "../../assets/filter.svg";
+import filterDropdown from "../../assets/filter-dropdown.svg";
+import calender from "../../assets/calender.svg";
+import editIcon from "../../assets/edit-icon.svg";
+import Loader from '../../components/loader/Loader';
+
+const JobCalculator = () => {
+  const [newFields, setNewFields] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [newFieldLabel, setNewFieldLabel] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+  const fileInputRef = useRef(null);
+  const [editingCell, setEditingCell] = useState({ row: null, column: null });
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(
+    [...Array(24)].map((_, idx) => ({
+      id: String(idx + 1).padStart(2, '0'),
+      room: '001',
+      width: '001',
+      height: '001',
+      type: '',
+      panel: 'OX',
+      quantity: '001',
+      price: '180',
+      additionalLabor: '-----',
+      notes: 'notes',
+      totalLabor: '001',
+    }))
+  );
+
+  const headers = ['Id', 'Room', 'Width', 'Height', 'Type', 'Panel', 'Quantity', 'Price', 'Additional Labor', 'Notes', 'Total Labor'];
+
+  const typeOptions = [
+    ' ',
+    'ES-EL100 - Single Hung',
+    'ES-EL200 - HORIZONTAL ROLLER',
+    'ES-EL200 - HORIZONTAL ROLLER XOX',
+    'ES-EL400 - SLIDING GLASS DOOR',
+    'ES-EL300 - SWING DOOR - SINGLE LEAF',
+    'ES-EL300 - SWING DOOR - DOUBLE LEAF',
+    'ES-EL150 SHAPE - FIXED WINDOW',
+    'MULLION',
+    'ES-EL300 - SWING DOOR - SINGLE LEAF with Side Lite',
+    'ES-EL300 - SWING DOOR - DOUBLE LEAF with side Lite',
+    'ES-EL300 - SWING DOOR - DOUBLE LEAF with 2 Side Lites',
+  ];
+
+  const handleUploadClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = async (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+
+      setLoading(true);
+      try {
+        const response = await axios.post('https://job-calculator-dan-01-hwa8c4czf7c6h5ec.westus-01.azurewebsites.net/uploadfile/', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+
+        const extractedData = response.data;
+        console.log('Extracted Data:', extractedData);
+        if (extractedData) {
+          const updatedData = extractedData.map((item, idx) => ({
+            id: String(idx + 1).padStart(2, '0'),
+            ...item,
+            notes: item.notes || '0',
+            totalLabor: item.totalLabor || '0',
+          }));
+          setData(updatedData);
+        }
+      } catch (error) {
+        console.error('Error uploading file:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
+  const [formData, setFormData] = useState({
+    municipality: '',
+    contractTotal: '',
+    engineeringCost: '',
+    commissionableAmount: '',
+    permit: '',
+    laborCost: '',
+    creditCardFees: '',
+    materialAmount: '',
+    shutterCost: '',
+    materialTax: '',
+    caulkingScrews: '',
+    commissionPercentage: '',
+    scaffold: '',
+    commissionAmount: '',
+    miscellaneous: '',
+    profitPercentage: '',
+    water: '',
+    jobProfit: '',
+  });
+
+  const handleSaveChange = () => {
+    setIsEditing(false);
+  };
+
+  const handleCellClick = (rowIdx, column) => {
+    if (column !== 'id') {
+      setEditingCell({ row: rowIdx, column });
+    }
+  };
+
+  const handleInputChange = (e, rowIdx, column) => {
+    const updatedData = [...data];
+    updatedData[rowIdx][column] = e.target.value;
+    setData(updatedData);
+  };
+
+  const handleBlur = () => {
+    setEditingCell({ row: null, column: null });
+  };
+
+  const handleAddField = () => {
+    setShowModal(true);
+  };
+
+  const handleConfirmAddField = () => {
+    if (newFieldLabel) {
+      setNewFields((prevFields) => [...prevFields, { label: newFieldLabel, value: '' }]);
+      setNewFieldLabel('');
+    }
+    setShowModal(false);
+  };
+
+  return (
+    <div className="flex min-h-screen xl:w-full lg:w-[700px] md:w-[460px] w-80 p-0 lg:mt-0 mt-4">
+      <div className="flex-1 p-6 bg-gray-50 overflow-auto pt-0">
+        <div className="flex lg:flex-row flex-col justify-between items-center mb-6">
+          <h1 className="text-2xl font-bold">Job Calculator</h1>
+          <div className="flex items-center justify-center space-x-4 h-[50px] mt-4 md:mt-0">
+            <div className="relative w-full max-w-[238px]">
+              <img
+                src={searchIcon}
+                alt="Search Icon"
+                className="absolute inset-y-0 left-0 pl-3 mt-3 flex items-center pointer-events-none"
+              />
+              <input
+                type="text"
+                placeholder="Search Job"
+                className="border border-gray-300 rounded-lg py-2 pl-10 pr-4 w-full"
+              />
+            </div>
+            <button className="bg-[#7234D7] text-white xl:px-4 xl:py-2 px-2 py-1 rounded-md xl:text-base text-sm">
+              Clone Job Calculator
+            </button>
+            <button
+              className="bg-[#7234D7] text-white xl:px-4 xl:py-2 px-2 py-1 rounded-md xl:text-base text-sm"
+              onClick={handleUploadClick}
+            >
+              Upload File
+            </button>
+            <input
+              type="file"
+              ref={fileInputRef}
+              onChange={handleFileChange}
+              style={{ display: 'none' }}
+            />
+          </div>
+        </div>
+
+        <div className='bg-white shadow-md rounded-lg p-4'>
+          <div className="flex xl:flex-row flex-col md:gap-3 gap-2 justify-between items-center mb-6">
+            <div className="flex items-center space-x-4">
+              <div className="flex items-center">
+                <span className="mr-2 text-[#666666]">Show</span>
+                <select className="border border-[#666666] rounded-lg xl:py-2 xl:px-3 px-2 py-1">
+                  <option value="24">24</option>
+                  <option value="50">50</option>
+                  <option value="100">100</option>
+                </select>
+                <span className="lg:ml-2 lg:mr-4  text-[#666666]">Entities</span>
+              </div>
+            </div>
+            <div className='flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2'>
+              <button className="bg-[#7234D7] border border-[#666666] text-white xl:px-4 xl:py-2 px-2 py-1 rounded-lg flex items-center justify-center gap-2 xl:text-base text-sm">
+                <img src={listView} alt='listView' />List View
+              </button>
+              <button className="border border-[#B3B3B3] px-4 py-2 rounded-lg flex items-center justify-center gap-2 xl:text-base text-sm">
+                <img src={kanbanView} alt='kanbanView' />Kanban View
+              </button>
+              <button className="border border-[#B3B3B3] px-4 py-2 rounded-lg flex items-center justify-center gap-2 xl:text-base text-sm">
+                <img src={filter} alt='filter' />Filter
+              </button>
+              <button className="border border-[#B3B3B3] px-4 py-2 rounded-lg flex items-center justify-center gap-2 xl:text-base text-sm">
+                <img src={calender} alt='calendar' />September 2024
+                <img src={filterDropdown} alt='filterDropdown' />
+              </button>
+            </div>
+          </div>
+
+          {loading ? (
+            <Loader />
+          ) : (
+            <div className="bg-white overflow-x-auto shadow-md rounded-lg p-4">
+              <div className="bg-white overflow-hidden w-full h-full">
+                <div className="max-h-[500px] overflow-y-auto w-full">
+                  <table className="min-w-full table-auto divide-y divide-gray-200 text-center">
+                    <thead className="bg-[#F4F7F9] rounded-xl">
+                      <tr>
+                        {headers.map((header) => (
+                          <th key={header} className="py-2 px-4 text-center text-sm font-medium tracking-wider">
+                            {header}
+                          </th>
+                        ))}
+                      </tr>
+                    </thead>
+                    <tbody className="bg-white divide-y divide-gray-200">
+                      {data.map((row, rowIdx) => (
+                        <tr key={rowIdx} className={rowIdx % 2 !== 0 ? 'bg-gray-100' : ''}>
+                          {Object.keys(row).map((column, colIdx) => (
+                            // <td key={colIdx} className="px-3 py-4 whitespace-nowrap" onClick={() => handleCellClick(rowIdx, column)}>
+                            //   {column === 'type' ? (
+                            //     <select
+                            //       value={row[column]}
+                            //       onChange={(e) => handleInputChange(e, rowIdx, column)}
+                            //       className="form-select px-4 py-2 rounded-md w-64 border border-gray-300"
+                            //     >
+                            //       {typeOptions.map((option, idx) => (
+                            //         <option key={idx} value={option}>
+                            //           {option}
+                            //         </option>
+                            //       ))}
+                            //     </select>
+                            //   ) : column === 'id' ? (
+                            //     <span>{row[column]}</span>
+                            //   ) : editingCell.row === rowIdx && editingCell.column === column ? (
+                            //     <input
+                            //       type="text"
+                            //       value={row[column]}
+                            //       onChange={(e) => handleInputChange(e, rowIdx, column)}
+                            //       onBlur={handleBlur}
+                            //       autoFocus
+                            //       className="px-2 py-1 w-16 border border-gray-300 rounded"
+                            //     />
+                            //   ) : (
+                            //     (row[column] === '' ? '0' : row[column])
+                            //   )}
+                            // </td>
+
+                            <td key={colIdx} className="px-3 py-4 whitespace-nowrap" onClick={() => handleCellClick(rowIdx, column)}>
+                              {column === 'type' ? (
+                                <select
+                                  value={typeOptions.includes(row[column]) ? row[column] : ' '}  // Check if the value exists in typeOptions, otherwise set to default empty option
+                                  onChange={(e) => handleInputChange(e, rowIdx, column)}
+                                  className="form-select px-4 py-2 rounded-md w-64 border border-gray-300"
+                                >
+                                  {typeOptions.map((option, idx) => (
+                                    <option key={idx} value={option}>
+                                      {option}
+                                    </option>
+                                  ))}
+                                </select>
+                              ) : column === 'id' ? (
+                                <span>{row[column]}</span>
+                              ) : editingCell.row === rowIdx && editingCell.column === column ? (
+                                <input
+                                  type="text"
+                                  value={row[column]}
+                                  onChange={(e) => handleInputChange(e, rowIdx, column)}
+                                  onBlur={handleBlur}
+                                  autoFocus
+                                  className="px-2 py-1 w-16 border border-gray-300 rounded"
+                                />
+                              ) : (
+                                (row[column] === '' ? '0' : row[column])
+                              )}
+                            </td>
+
+                          ))}
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div>
+          <div className="grid xl:grid-cols-2 lg:grid-cols-2 grid-cols-1 gap-2 mt-3 p-4 ">
+            {[
+              'Municipality', 'Contract Total', 'Engineering Cost', 'Commissionable Amount', 'Permit',
+              'Labor Cost', 'Credit Card Fees', 'Material Amount', 'Shutter Cost', 'Material Tax',
+              'Caulking Screws', 'Commission Percentage', 'Scaffold', 'Commission Amount', 'Miscellaneous',
+              'Profit Percentage', 'Water', 'Job Profit'
+            ].map((label, idx) => (
+              <div key={idx} className="flex lg:flex-row md:flex-row flex-col lg:items-center md:items-center gap-2">
+                <label className="w-44 text-sm text-gray-700 capitalize font-semibold">{label}</label>
+                {isEditing ? (
+                  <input
+                    type="text"
+                    placeholder="Enter value"
+                    value={formData[label.toLowerCase()]}
+                    onChange={(e) => setFormData({ ...formData, [label.toLowerCase()]: e.target.value })}
+                    className="flex-1 border border-gray-300 rounded-lg py-2 px-4 mt-1"
+                  />
+                ) : (
+                  <span className="flex-1 py-2 px-4 border border-gray-300 rounded-lg mt-1 text-gray-700">
+                    {formData[label.toLowerCase()] || 'Enter Value'}
+                  </span>
+                )}
+              </div>
+            ))}
+
+            {newFields.map((field, idx) => (
+              <div key={idx + 100} className="flex items-center gap-2">
+                <label className="w-44 text-sm text-gray-700 capitalize font-semibold">{field.label}</label>
+                {isEditing ? (
+                  <input
+                    type="text"
+                    placeholder="Enter value"
+                    value={field.value}
+                    onChange={(e) => {
+                      const updatedFields = [...newFields];
+                      updatedFields[idx].value = e.target.value;
+                      setNewFields(updatedFields);
+                    }}
+                    className="flex-1 border border-gray-300 rounded-lg py-2 px-4 mt-1"
+                  />
+                ) : (
+                  <span className="flex-1 py-2 px-4 border border-gray-300 rounded-lg mt-1 text-gray-700">
+                    {field.value || 'Enter Value'}
+                  </span>
+                )}
+              </div>
+            ))}
+          </div>
+
+          <div className="flex items-center gap-4 mt-5">
+            {isEditing ? (
+              <>
+                <button
+                  className="bg-[#7234D7] text-white px-4 py-2 rounded"
+                  onClick={handleSaveChange}
+                >
+                  Save Changes
+                </button>
+                <button
+                  className="bg-gray-400 text-white px-4 py-2 rounded"
+                  onClick={handleAddField}
+                >
+                  Add Field
+                </button>
+              </>
+            ) : (
+              <button
+                className="flex items-center gap-2 border border-gray-400 rounded px-5 py-2"
+                onClick={() => setIsEditing(true)}
+              >
+                <img src={editIcon} alt="edit icon" />
+                <span className="text-[#7234D7]">Edit</span>
+              </button>
+            )}
+          </div>
+
+          {showModal && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30">
+              <div className="bg-white p-6 rounded shadow-lg">
+                <h3 className="text-lg font-semibold mb-3">Add New Field</h3>
+                <input
+                  type="text"
+                  placeholder="Enter field name"
+                  value={newFieldLabel}
+                  onChange={(e) => setNewFieldLabel(e.target.value)}
+                  className="border border-gray-300 rounded-lg py-2 px-4 mb-4 w-full"
+                />
+                <div className="flex justify-end gap-4">
+                  <button
+                    className="bg-gray-300 text-gray-700 px-4 py-2 rounded"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="bg-[#7234D7] text-white px-4 py-2 rounded"
+                    onClick={handleConfirmAddField}
+                  >
+                    Add Field
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default JobCalculator;
